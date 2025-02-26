6826da3f5b9ec93f15f1e6e9e0ebcd6a0e464436
Mukwevho Lufuno <99746961+LufunoMiles@users.noreply.github.com>
2025-02-26 15:58:46 +0200

    Create MoiveQoute.java

---
+++ MoiveQoute.java
@@ -0,0 +1,16 @@
+//1.display fav movies qoutes
+//2.Name of the movie it comes from 
+//3.Charecter who said it 
+//4.The year  of the moive release 
+
+public class MoiveQoute{
+    public static void main(String[] args) {
+         System.out.println("Fav Qoute :Now, I know what people mean when they say, 'I wish I knew then what I know now.' Ain't that the truth? Probably save me a whole lot of pain and suffering.");
+      System.out.println(" Name of the Moive:I'm in Love with a Church Girl");
+      System.out.println("Charecter: Miles Montego");
+      System.out.println("Year released :2013");
+    }
+}
+
+
+
